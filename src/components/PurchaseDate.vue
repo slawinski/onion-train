683d1c543558ec0ebacd8149456baabff0393b19
piotr-s <piotr.slawinski1@gmail.com>
2019-03-16 16:52:17 +0100

    Add date component

---
+++ src/components/PurchaseDate.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Train ticket reminder</h1>
+    <label for="date">Departure date: </label>
+    <input name="date" type="date" v-model="desiredDate" />
+    <p>Ticket purchase date: {{ purchaseDate }}</p>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  data() {
+    return {
+      desiredDate: null,
+    };
+  },
+  computed: {
+    purchaseDate: function() {
+      return moment(this.desiredDate)
+        .subtract(30, 'days')
+        .format('DD-MMM-YYYY');
+    },
+  },
+};
+</script>
+
+<style></style>
