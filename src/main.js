2c3945d9e800a6d6ff73b6abd0d7957fc93d6a54
piotr-s <piotr.slawinski1@gmail.com>
2019-03-22 22:05:25 +0100

    Version control is hard!

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import AddToCalendar from 'vue-add-to-calendar';
+
+Vue.use(AddToCalendar);
+
 Vue.config.productionTip = false;
 
 new Vue({
