c390175a2a3f1c83b49f9cbc1988a13e2226839b
piotr-s <piotr.slawinski1@gmail.com>
2019-03-22 22:00:46 +0100

    Install calendar library

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-import AddToCalendar from 'vue-add-to-calendar';
-
-Vue.use(AddToCalendar);
-
 Vue.config.productionTip = false;
 
 new Vue({
