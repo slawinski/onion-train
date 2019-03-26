29187bfed9b9144847e6c922fff87b4ee829f9a8
piotr-s <piotr.slawinski1@gmail.com>
2019-03-26 17:38:06 +0100

    Another 'about' update

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,12 +2,12 @@
   <div>
     <p><strong>The Onion Train</strong> was built of need.</p>
     <p>
-      Tickets for Intercity Premium trains are fucking expensive. The only way to enjoy your ride
-      and not ruin yourself financially is to buy a ticket 30 days before the trip.
+      Tickets for the Intercity Premium trains are fucking expensive. The only way to enjoy your
+      ride and not ruin yourself financially is to buy a ticket 30 days before the trip.
     </p>
     <p>
-      This app will remind you to stay late on that day as you need to buy the tickets immediately
-      after they become available, before the discounted ones ran out.
+      This app will remind you to stay late on that day as you need to buy the discounted tickets
+      immediately after they become available, before they ran out.
     </p>
     <p>
       "Even if you're on the right track, you'll get run over if you just sit there." ~ Will Rogers
